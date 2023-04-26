9b066797853912d28e48875f2c0548e406d9fb84
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-26 15:09:16 +0200

    PHP create Table 'Types' with Migration

--- database/migrations/2023_04_26_130219_create_types_table.php
+++ database/migrations/2023_04_26_130219_create_types_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 150)->unique();
+            // Campo Univoco generato dal Nome come riconoscimento al posto di ID
+            $table->string('slug');
             $table->timestamps();
         });
     }
