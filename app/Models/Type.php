8fc583c954b3dddab72ebc4cb3ae3f0b27983c1b
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-26 15:52:45 +0200

    PHP create Relationship: Type [1:N] Project(s)

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+
+    // Indico una Relazione: Type [1:N] Project(s), cioe' una Tipologia puo' essere assegnata a piu' Progetti 
+    public function projects()
+    {
+
+        return $this->hasMany(Project::class);
+    }
 }
